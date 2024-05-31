a07f5b4207a8df413ffa4b5b9030d7ddb2a94661
EliezarGongora <eliezar.gongora@gmail.com>
2024-05-31 10:36:18 -0600

    Final Project

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -3,7 +3,59 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation2 World!");
-    }
+    {   
+        Console.WriteLine("**************************************************");
+        
+        Address address1 = new Address ("Col. Santa Isabela #6 ", "Zacate", "LP", "El Salvador");
+        Customer customer1 = new Customer("Eliezar", address1);
+
+        double order1ShippingCost = customer1.GetShippingCost();
+
+        Product order1Product1 = new Product("Pupusas", "201", 0.75, 4);
+        Product order1Product2 = new Product("Mangos", "4225", 0.50, 6);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(order1Product1);
+        order1.AddProduct(order1Product2);
+        double order1Subtotal = order1.CalculateSubtotal();
+        double order1Total = order1.CalculateTotal();
+
+        
+        order1.DisplayShippingLabel(); 
+        Console.WriteLine();
+        order1.DisplayPackingLabel();
+        Console.WriteLine();
+        order1.DisplayCosts(order1Subtotal ,order1ShippingCost, order1Total);
+
+
+        Console.WriteLine("**************************************************");
+
+
+        
+        Address address2 = new Address ("1234 Marshmellow Dr.", "Boise", "ID", "USA");
+        Customer customer2 = new Customer("Kimberly", address2);
+
+        double order2ShippingCost = customer2.GetShippingCost();
+
+        Product order2Product1 = new Product("Socks", "4260", 2.00, 5);
+        Product order2Product2 = new Product("Nike Cortez", "4959", 100.00, 1);
+        Product order2Product3 = new Product("Iphone 15", "4029", 1500.00, 1);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(order2Product1);
+        order2.AddProduct(order2Product2);
+        order2.AddProduct(order2Product3);
+        double order2Subtotal = order2.CalculateSubtotal();
+        double order2Total = order2.CalculateTotal();
+
+
+        
+        order2.DisplayShippingLabel(); 
+        Console.WriteLine();
+        order2.DisplayPackingLabel();
+        Console.WriteLine();
+        order2.DisplayCosts(order2Subtotal, order2ShippingCost, order2Total);
+        Console.WriteLine("**************************************************");
+
+    }   
 }
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address ("Col. Santa Isabela #6 ", "Zacate", "LP", "57214", "El Salvador");
+        string lectureAddress = address1.GetAddress();
+        Lectures lecture = new Lectures("Stake Conference", "Church meeting with members of the San Vicente Stake.", "June 15, 2024", "10:00 am - 12:00 pm", $"{lectureAddress}", "Lecture", "Elder Gongora", 4000);
+
+       
+        Console.WriteLine("**************************************************");
+        lecture.DisplayShortDescription();
+        Console.WriteLine();
+        lecture.DisplayStandardDetails();
+        Console.WriteLine();
+        lecture.DisplayLectureFullDetails();
+
+
+        Address address2 = new Address ("1234 Marshmellow Dr.", "Boise", "ID", "83125","USA");
+        string receptionAddress = address2.GetAddress();
+        Receptions reception = new Receptions("Eliezar And Kimberly", "Eliezar And Kimberly's wedding ceremony and reception", "September 15, 2018", "10:00 am - 06:00pm", $"{receptionAddress}", "Reception", "enjoyourwedding@gmail.com");
+        
+
+        Console.WriteLine("**************************************************");
+        reception.DisplayShortDescription();
+        Console.WriteLine();
+        reception.DisplayStandardDetails();
+        Console.WriteLine();
+        reception.DisplayReceptionsFullDetails();
+
+
+        Address address3 = new Address ("789 W Landers Ave.", "Hailey", "ID", "43017", "USA");
+        string outdoorAddress = address3.GetAddress();
+        Outdoor outdoor = new Outdoor("Game Day Activity", "Sports at the park for all youth.", "February 31, 1995", "6:00 pm", $"{outdoorAddress}", "Outdoor", "Sunny");
+       
+       
+
+        Console.WriteLine("**************************************************");
+        outdoor.DisplayShortDescription();
+        Console.WriteLine();
+        outdoor.DisplayStandardDetails();
+        Console.WriteLine();
+        outdoor.DisplayOutdoorFullDetails();
+        Console.WriteLine("**************************************************");
+        
     }
 }
